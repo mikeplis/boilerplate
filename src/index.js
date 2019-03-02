870bda6ee2a22da60beaadef338e420f7d2c2517
Mike Plis <michael.plis01@gmail.com>
2019-03-02 17:55:07 -0500

    Set up eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const [state, setState] = React.useState(0);
-    return <button onClick={() => setState(state + 1)}>{state}</button>;
+    return (
+        <div>
+            <button onClick={() => setState(state + 1)}>{state}</button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
