218ce5d7641f724b20992d790794fc54ccdd1fc6
Mike Plis <michael.plis01@gmail.com>
2019-03-02 17:25:22 -0500

    Set up webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => React.createElement('div', {}, 'Hello World');
+const App = () => {
+    const [state, setState] = React.useState(0);
+    return React.createElement(
+        'button',
+        {
+            onClick: () => setState(state + 1)
+        },
+        state
+    );
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     }
 };
